c5252f560652885369587467f27218349048e218
koparasy <koparasy@linux-c88h.site>
2014-11-14 19:25:32 +0200

    changes to allow fault campaings in labs

--- src/fi/loadstore_injfault.hh
+++ src/fi/loadstore_injfault.hh
@@ -9,36 +9,43 @@
 
 class LoadStoreInjectedFault : public O3CPUInjectedFault
 {
-public:
-  
-  LoadStoreInjectedFault(std::ifstream &os);
-  ~LoadStoreInjectedFault();
-  
-  virtual TheISA::MachInst process(TheISA::MachInst inst) { std::cout << "O3CPUInjectedFault::manifest() -- virtual\n"; assert(0); return inst;};
-  virtual StaticInstPtr process(StaticInstPtr inst) { std::cout << "O3CPUInjectedFault::manifest() -- virtual\n"; assert(0); return inst;};
-  virtual StaticInstPtr process(StaticInstPtr inst , int regNum) { std::cout << "O3CPUInjectedFault::manifest() -- virtual\n"; assert(0); return inst;};
-  
-  
-  virtual const char *description() const;
-  void dump() const;
-  
-  template <class T> 
-  T process(T v, unsigned dataSize){
-    DPRINTF(FaultInjection,"===LoadStoreFault::Process(T)==\n");
-    
-    T retval = v;
-    if(getValueType() == InjectedFault::FlipBit && getValue() > dataSize*8){ //Make sure that the flipped bit is inside the affected structure!
-      setValue(getValue()%(dataSize*8) +1);
-      DPRINTF(FaultInjection,"Altered Flip bit location\n");
-      dump();
-    }
-    retval = manifest(v,getValue(),getValueType());
-    
-    check4reschedule();
-    
-    DPRINTF(FaultInjection,"~===LoadStoreFault::Process(T)===\n");
-    return retval;
-  }
+	public:
+
+		LoadStoreInjectedFault(std::ifstream &os);
+		~LoadStoreInjectedFault();
+
+		virtual StaticInstPtr process(StaticInstPtr inst) { std::cout << "O3CPUInjectedFault::manifest() -- virtual\n"; assert(0); return inst;};
+		virtual StaticInstPtr process(StaticInstPtr inst , int regNum) { std::cout << "O3CPUInjectedFault::manifest() -- virtual\n"; assert(0); return inst;};
+
+
+		virtual const char *description() const;
+		void dump() const;
+
+		template <class T> 
+			T process(T v){
+				DPRINTF(FaultInjection,"===LoadStoreFault::Process(T)==\n");
+
+				T retval = v;
+				retval = manifest(v,getValue(),getValueType());
+
+				check4reschedule();
+
+				DPRINTF(FaultInjection,"~===LoadStoreFault::Process(T)===\n");
+				return retval;
+			}
+
+		virtual TheISA::MachInst process(uint64_t v) { 
+			DPRINTF(FaultInjection,"===LoadStoreFault::Process(uint64_t)==\n");
+
+			uint64_t retval = v;
+			retval = manifest(v,getValue(),getValueType());
+
+			check4reschedule();
+
+			DPRINTF(FaultInjection,"~===LoadStoreFault::Process(uint64_4)===\n");
+			return retval;
+		}
+
 };
 
 
