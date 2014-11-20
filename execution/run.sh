a7b7aba916a0d130ecfdce215bac3318d496a332
koparasy <koparasy@linux-c88h.site>
2014-11-20 23:04:21 +0200

    pushing new run sh

--- execution/run.sh
+++ execution/run.sh
@@ -7,11 +7,11 @@ terminate(){
 }
 
 #global variabled to handle storage units
-export M5_PATH=/scratch/x86
+export M5_PATH=/home/dinos/x86
 checkpoint_dir="my_ckpts"
-results="/scratch/results"
-all_exp="/scratch/experiments"
-mutex="/scratch/script.lock"
+results="/home/dinos/results"
+all_exp="/home/dinos/experiments"
+mutex="/home/dinos/script.lock"
 experiments=(Fetch.txt Decode.txt IEW.txt LDS.txt)
 ckpts=(fetch_ckpt.dmtcp decode_ckpt.dmtcp iew_ckpt.dmtcp lds_ckpt.dmtcp)
 ckpt_dirs=(fetch decode iew lds)
@@ -62,40 +62,21 @@ dmtcp_checkpoint ./../../build/X86/gem5.opt --debug-flags=FaultInjection --remot
   result=$?
   if [ "$result" -eq "1" ]; then
     echo "Checkpoint created">>"$my_core"
-    cp maincheckpoint.dmtcp "$checkpoint_dir/maincheckpoint/"
-    cp -r ckpt_gem5.opt_* "$checkpoint_dir/maincheckpoint/"
-    rm dmtcp_restart*
-    rm maincheckpoint.dmtcp
-    rm -r ckpt_gem5.opt_*
+    #cp maincheckpoint.dmtcp "$checkpoint_dir/maincheckpoint/"
+     #   cp -r ckpt_gem5.opt_* "$checkpoint_dir/maincheckpoint/"
+     #   rm dmtcp_restart*
+     #   rm maincheckpoint.dmtcp
+     # rm -r ckpt_gem5.opt_*
   else
     if [ -f dmtcp_restart*.sh ]; then
       rm dmtcp_restart*.sh
     fi
-    if [ -f ckpt_gem5.opt* ]; then
-      rm ckpt_gem5.opt*
-    fi
+#    if [ -f ckpt_gem5.opt* ]; then
+# rm ckpt_gem5.opt*
+#    fi
     echo "ERROR IN CREATING THE CHECKPOINT $my_core"
   fi
 fi
-
-if [ -f "input" ]; then
-      cp $checkpoint_dir/maincheckpoint/maincheckpoint.dmtcp .
-      cp -r $checkpoint_dir/maincheckpoint/ckpt_gem5.opt_* .
-      dmtcp_restart maincheckpoint.dmtcp &
-      pids=$!
-      echo "Passing child id $i"
-      ./../time.sh $pids
-      _name=$(echo -n  "$(/sbin/ifconfig | grep 'eth1\|eth0\|eth2\|eth3' | tr -s ' '  | cut -d ' ' -f5| sed 's/://g')$(date | sed 's/ //g')$my_core")
-      echo  "$my_core storing $_name ..... " >>"$my_core"
-      echo "$_name"
-      mkdir "$_name"
-      mv input "$_name"
-      cp start/* "$_name"
-      rm start/ApplicationOutput 
-      rm *.dmtcp
-      rm -r ckpt_gem5.opt*
-fi
-
 
 for (( i = 0 ; i < ${#experiments[@]} ; i++))
 do
@@ -103,39 +84,39 @@ do
   echo "$cur_exp"
   while [ -s "$cur_exp" ]
   do
-    
-    echo "Lock taken"
-    lockfile $mutex #take lock
-    echo "Lock taken"
-    exp=$(tail -n 1 $cur_exp)
-    head -n -1 $cur_exp > temp.txt 
-    mv temp.txt $cur_exp
-    
-    rm -f $mutex #free lock
-    echo "lock released"
-    echo -n "$exp">input
 
+    if [ ! -f input ]; then  
+      echo "Lock taken"
+      lockfile $mutex #take lock
+      echo "Lock taken"
+      exp=$(tail -n 1 $cur_exp)
+      head -n -1 $cur_exp > temp.txt 
+      mv temp.txt $cur_exp
+      rm -f $mutex #free lock
+      echo "lock released"
+      echo -n "$exp">input
+    fi
 # Check if there is a checkpoint for this kind of faults
 # if there is use it otherwise use the maincheckpoint
 
-    if [ -f "$checkpoint_dir/${ckpt_dirs[$i]}/${ckpts[$i]}" ]; then
+    if [ -f "${ckpts[$i]}" ]; then
       echo "Restoring from $ckpts[$i]"
-      cp $checkpoint_dir/${ckpt_dirs[$i]}/${ckpts[$i]} . 
-      cp -r $checkpoint_dir/${ckpt_dirs[$i]}/ckpt_gem5.opt_* .
+#cp $checkpoint_dir/${ckpt_dirs[$i]}/${ckpts[$i]} . 
+#     cp -r $checkpoint_dir/${ckpt_dirs[$i]}/ckpt_gem5.opt_* .
       dmtcp_restart ${ckpts[$i]} &
       pids=$!
       echo "Passing child id $i"
       ./../time.sh $pids
     else
       echo "restoring from maincheckpoint"
-      cp $checkpoint_dir/maincheckpoint/maincheckpoint.dmtcp .
-      cp -r $checkpoint_dir/maincheckpoint/ckpt_gem5.opt_* .
+#      cp $checkpoint_dir/maincheckpoint/maincheckpoint.dmtcp .
+#      cp -r $checkpoint_dir/maincheckpoint/ckpt_gem5.opt_* .
       dmtcp_restart maincheckpoint.dmtcp &
       pids=$!
       echo "Passing child id $i"
       ./../time.sh $pids
     fi
-    _name=$(echo -n  "$(/sbin/ifconfig | grep 'eth1\|eth0\|eth2\|eth3' | tr -s ' '  | cut -d ' ' -f5| sed 's/://g')$(date | sed 's/ //g')$my_core")
+    _name=$(echo -n  "$(/sbin/ifconfig | grep 'em0\|em1\|eth1\|eth0\|eth2\|eth3' | tr -s ' '  | cut -d ' ' -f5| sed 's/://g')$(date | sed 's/ //g')$my_core")
     echo  "$my_core storing $_name ..... " >>"$my_core"
     echo "$_name"
     mkdir "$_name"
@@ -146,14 +127,12 @@ do
     echo "$ my_core .....results stored">>"$my_core"
      
 
-    if [ -f "${ckpts[$i]}" ]; then
-     cp -ri ${ckpts[$i]}  "$checkpoint_dir/${ckpt_dirs[$i]}/"
-     rm	 ${ckpts[$i]}
-     cp -r ckpt_gem5.opt_*  "$checkpoint_dir/${ckpt_dirs[$i]}/"
-	rm -r  ckpt_gem5.opt_* 
-    else
-      echo "This should never happen"
-    fi
+#    if [ -f "${ckpts[$i]}" ]; then
+#     mv ${ckpts[$i]}  "$checkpoint_dir/${ckpt_dirs[$i]}/"
+#     mv ckpt_gem5.opt_*  "$checkpoint_dir/${ckpt_dirs[$i]}/"
+#    else
+#      echo "This should never happen"
+#    fi
   done
 done
 #trap "terminate $pids" SIGINT SIGTERM
